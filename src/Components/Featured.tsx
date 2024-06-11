3d0f1e92903a711df01222c7166af1dbc50c8b75
Stephen Olasunkanmi <bonniedevis727@gmail.com>
2024-06-11 21:30:16 +0100

    feature

--- src/Components/Featured.tsx
+++ src/Components/Featured.tsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Image from 'next/image'
+
 
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className='w-screen overflow-x-scroll text-red-500'>
+        {/* Wrapper */}
+        <div className="w-max flex">
+            {/* SINGLE ITEM */}
+            <div className="w-screen h-[60vh] flex flex-col items-center justify-around">
+                {/* iMAGE CONTAINER */}
+                <div className="relative">
+                    <Image src="" alt="" fill/>
+                </div>
+                {/* TEXT CONTAINER */}
+                <div>
+                    <h1 className="">Title</h1>
+                    <p>Desc</p>
+                    <span>123</span>
+                    <button>Add to cart</button>
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
--- src/Components/Slider.tsx
+++ src/Components/Slider.tsx
@@ -32,16 +32,16 @@ const Slider = () => {
     }, []);
 
     return (
-        <div className='flex flex-col h-screen lg:flex-row lg:h-full'>
+        <div className='flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row bg-fuchsia-50'>
             {/* TEXT CONTAINER */}
-            <div className=" flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
-                <h1 className="text-5xl text-center uppercase p-4 md:text-6xl xl:text-7xl">
+            <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
+                <h1 className="text-5xl text-center uppercase p-4 md:text-6xl md:p=10 xl:text-7xl">
                     {data[currentSlide].title}
                 </h1>
                 <button className="bg-red-500 text-white py-4 px-8">Order Now</button>
             </div>
             {/* IMAGE CONTAINER */}
-            <div className="w-full relative lg:h-full">
+            <div className="flex-1 w-full relative ">
                 <Image src={data[currentSlide].image} alt={`Slide ${data[currentSlide].id}`} layout="fill" className='object-cover' />
             </div>
         </div>
