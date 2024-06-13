0e1bebcc061ba88960dc60a2ebbc7644004b67ca
Stephen Olasunkanmi <bonniedevis727@gmail.com>
2024-06-13 10:57:18 +0100

    offer

--- src/Components/Featured.tsx
+++ src/Components/Featured.tsx
@@ -1,29 +1,33 @@
-import React from 'react'
-import Image from 'next/image'
-
+import React from 'react';
+import Image from 'next/image';
+import { featuredProducts } from '@/data';
 
 const Featured = () => {
-  return (
-    <div className='w-screen overflow-x-scroll text-red-500'>
-        {/* Wrapper */}
-        <div className="w-max flex">
-            {/* SINGLE ITEM */}
-            <div className="w-screen h-[60vh] flex flex-col items-center justify-around">
-                {/* iMAGE CONTAINER */}
-                <div className="relative">
-                    <Image src="" alt="" fill/>
-                </div>
-                {/* TEXT CONTAINER */}
-                <div>
-                    <h1 className="">Title</h1>
-                    <p>Desc</p>
-                    <span>123</span>
-                    <button>Add to cart</button>
-                </div>
+    return (
+        <div className='w-screen overflow-x-scroll text-red-500'>
+            {/* Wrapper */}
+            <div className="w-max flex">
+                {/* SINGLE ITEM */}
+                {featuredProducts.map(item => (
+                    <div key={item.id} className="w-screen h-[60vh] flex flex-col items-center justify-around hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[100vh] py-6">
+                        {/* IMAGE CONTAINER */}
+                        {item.img && (
+                            <div className="relative flex-1 w-full hover:rotate-[80deg] transition-all duration-500">
+                                <Image src={item.img} alt={item.title} fill className='object-contain' />
+                            </div>
+                        )}
+                        {/* TEXT CONTAINER */}
+                        <div className='flex-1 flex flex-col gap-4 items-center justify-center'>
+                            <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">{item.title}</h1>
+                            <p className='p-4 text-center 2xl:p-8'>{item.desc}</p>
+                            <span>${item.price}</span>
+                            <button className='bg-red-500 text-white py-2 px-4'>Add to cart</button>
+                        </div>
+                    </div>
+                ))}
             </div>
         </div>
-    </div>
-  )
-}
+    );
+};
 
-export default Featured
+export default Featured;
--- src/Components/Offer.tsx
+++ src/Components/Offer.tsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import Image from 'next/image'
 
 const Offer = () => {
   return (
-    <div>Offer</div>
+    <div className='bg-black h-screen flex flex-col md:flex-row'>
+      {/* TEXT CONTAINER */}
+      <div className="flex-1 flex flex-col justify-center items text-center gap-8 p-6">
+          <h1 className="text-white font-bold xl:text-6xl">Delicious Burger & French Fry</h1>
+          <p className="text-white">Progressively simplify effective e-toilers and process-centric methods of empowerment. Quickly pontificate parallel.</p>
+          <button>Order Now</button>
+      </div>
+      {/* IMAGE CONTAINER */}
+      <div className="flex-1 w-full relative">
+        <Image src="/offerProduct.png" alt="" fill className='object-contain'/>
+      </div>
+    </div>
   )
 }
 
